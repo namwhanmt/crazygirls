f7f7db0a792ab31681603348c1a700f6ed5b1280
5710513021 <wipada_2538@hotmail.com>
2015-10-19 05:39:56 -0400

    I editting shell calculator.sh

--- shell_calculator.sh
+++ shell_calculator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Enter number"
+echo "Get number"
 read x1
 echo "What operation?"
 read x2
-echo "Enter number"
+echo "Get number"
 read x3
 echo "$x1 $x2 $x3"
 ans=$(expr "$x1" "$x2" "$x3")
