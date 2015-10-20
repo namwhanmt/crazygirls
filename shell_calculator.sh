77c605d625537c96f738e54420a89766902c2813
5710513021 <wipada_2538@hotmail.com>
2015-10-20 00:47:14 -0400

    I editting shell_calculator.sh

--- shell_calculator.sh
+++ shell_calculator.sh
@@ -2,10 +2,11 @@
 
 echo "Get number"
 read x1
-echo "What operation?"
+echo "Choose operation?"
 read x2
 echo "Get number"
 read x3
 echo "$x1 $x2 $x3"
 ans=$(expr "$x1" "$x2" "$x3")
 echo Answer : $ans
+:
